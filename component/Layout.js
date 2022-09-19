c428b65c1ffe6a1b6b8037ec3f6de56bb6bab149
yuvahree <107060025+yuvahree@users.noreply.github.com>
2022-09-19 13:53:49 +0530

    next.js

---
+++ component/Layout.js
@@ -0,0 +1,20 @@
+import styles from "../styles/Layout.module.css";
+import Nav from "./Nav";
+import Header from "./Header";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Nav />
+      <div className={styles.container}>
+        <main className={styles.main}>
+          {/* <h1>tggvb</h1> */}
+          <Header />
+          {children}
+        </main>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
